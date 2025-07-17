2d046d22c1ecc243714a95a00d30a3c838617f3f
Nancy <wangui.kanyingi2015@gmail.com>
2025-07-17 18:25:24 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((sentence) => {
+      const words = sentence.split(" ");
+      const upperCaseWords = words.map(
+        (word) => word.charAt(0).toUpperCase() + word.slice(1)
+
+      );
+      const result = upperCaseWords.join(" ");
+      return result;
+    });
+};
